e44b8fcfed218c340cd6d8c3308256a600f15b0a
Sabal Mary Jane <34365411+emjee@users.noreply.github.com>
2017-12-10 23:40:59 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head></head>
 	<body>
 		<div>
-		<img src = "fairytale_1600x900_0.jpg" width="100%" height="100%">
+		<img src = "classdiagram.png" width="100%" height="100%">
 		</div>
 	</body>
 </html>
